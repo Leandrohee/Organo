2a9785f34f7511a1a57f0a3d288459de97a7557a
Leandro Torres <leandrotorres@MacBook-Air-de-Leandro.local>
2023-06-01 11:40:02 -0300

    Coloquei alt na imagem e tirei useState da lista

--- src/components/CarrocelTimes/BoxLista/BoxLista.js
+++ src/components/CarrocelTimes/BoxLista/BoxLista.js
@@ -4,7 +4,7 @@ function BoxLista(props){
     return(
         <li className='box-lista'>
             <div style={{backgroundColor: props.colorDivMenor}} ></div>
-            <img src={props.src}/>
+            <img src={props.src} alt={props.h5}/>
             <h5>{props.h5}</h5>
             <p>{props.p}</p>
         </li>
--- src/components/ListaSuspensa/ListaSuspensa.js
+++ src/components/ListaSuspensa/ListaSuspensa.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "./ListaSuspensa.css"
 
 function ListaSuspensa(props){
